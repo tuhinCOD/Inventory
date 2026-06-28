385a90b4b711fb4ce0c73276b153f4c1bc93ac42
tuhin <mahabubhasantuhin2005@gmail.com>
2026-06-28 17:44:19 +0600

    first inventory commit

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+        vue({
+        template: {
+            transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+            },
+        },
+        }),
+        tailwindcss(),
+    ],
+    css: {
+        preprocessorOptions: {
+        scss: {
+            api: "modern-compiler", // or "modern",
+        },
+        },
+    },
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+        proxy: {
+            "/api": {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+                secure: false,
+                credentials: true,
+            },
+        },
+    },
+});
